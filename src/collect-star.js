435b6c35ab5ca87bb44ccf640c0a297547639ae8
Kai Chen <kaichenusesthis@gmail.com>
2017-04-03 20:58:38 +0000

    Added src folder and moved files

---
+++ src/collect-star.js
@@ -0,0 +1,13 @@
+/* global score */
+/* global scoreText */
+function collectStar (player, star) {
+    
+  
+    star.kill();
+    
+  
+
+    score += 10;
+    scoreText.text = 'Score: ' + score;
+
+}
---
+++ src/update.js
@@ -0,0 +1,50 @@
+
+function update() {
+
+  
+    game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(stars, platforms);
+    
+   
+   
+    
+    game.physics.arcade.overlap(player, stars, collectStar, null, null);
+
+
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+      
+        player.body.velocity.x = -150;
+
+        player.animations.play('left'); 
+        
+       
+        
+       
+    }
+    else if (cursors.right.isDown)
+    {
+
+        player.body.velocity.x = 150;
+
+        player.animations.play('right');
+        
+         
+    }
+    else
+    {
+
+        player.frame = 4;
+    }
+    
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
+    }
+    
+    
+
+}
