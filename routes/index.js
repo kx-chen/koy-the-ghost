abccb458d0ea602bb422529821ae33443e7bcf6b
Kai Chen <kaichenusesthis@gmail.com>
2017-05-09 17:36:10 +0000

    Express js app serves html now.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('game', { title: 'Game' });
+});
+
+router.get('/game', function(req, res) {
+   res.render('game', {title: "Game"}); 
 });
 
 module.exports = router;
