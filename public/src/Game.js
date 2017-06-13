3dea331f2a004b49a2431afb0c0606a86735cd4c
Kai Chen <kaichenusesthis@gmail.com>
2017-06-13 18:40:45 +0000

    Added helpful comments

--- public/src/Game.js
+++ public/src/Game.js
@@ -24,7 +24,11 @@ unitGame.Game.prototype.create = function() {
         // items.js
         this.createItems();
         
+        
+        // Iniates the space key. this.spacekey is referenced later in Update.js
         this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        
+        // Instantiates the keyboard
         this.cursors = this.game.input.keyboard.createCursorKeys();
   
 }
