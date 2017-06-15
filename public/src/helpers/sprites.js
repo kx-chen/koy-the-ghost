629d79bb2e8082c37a7bc23637adec411b184cda
Kai Chen <kaichenusesthis@gmail.com>
2017-06-15 18:21:53 +0000

    add comments

--- public/src/helpers/sprites.js
+++ public/src/helpers/sprites.js
@@ -34,7 +34,7 @@ unitGame.Game.prototype.addSprites = function() {
     
     
     for (var i = 0; i < 1200; i++) {
-       
+       // TODO: randomize enemy spawning
         this.enemy = this.enemies.create(i * 70, 1000, 'enemy');
 
         this.enemy.body.gravity.y = 300;
