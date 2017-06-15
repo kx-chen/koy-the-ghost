a47f26bcbfcf7f5131f5a738dbc241c69c181afc
Kai Chen <kaichenusesthis@gmail.com>
2017-06-15 18:10:02 +0000

    player collides with top of game

--- public/src/helpers/sprites.js
+++ public/src/helpers/sprites.js
@@ -9,13 +9,15 @@ unitGame.Game.prototype.addSprites = function() {
     // Adds in player
     this.player = this.game.add.sprite(32, 125, 'dude');
     this.player.scale.setTo(2,2);
+    this.player.enableBody = true;
+
 
     
     this.game.physics.arcade.enable(this.player);
 
     // this.player.body.bounce.y = 0.2;
     this.player.body.gravity.y = 1000;
-    this.player.body.collideWorldBounds = false;
+    this.player.body.collideWorldBounds = true;
     
         
     this.player.animations.add('right', [0, 1], 2, true);
