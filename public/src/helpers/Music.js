c5ec2e2418e48db0515e9cccdd63f5395d527368
Kai Chen <kaichenusesthis@gmail.com>
2017-06-17 05:36:41 +0000

    plays only one music track now

--- public/src/helpers/Music.js
+++ public/src/helpers/Music.js
@@ -4,11 +4,11 @@
   
   // TODO: don't repeat self
      this.soundtrack1 = this.game.add.audio('soundtrack1');
-     this.soundtrack2 = this.game.add.audio('soundtrack2');
+     // this.soundtrack2 = this.game.add.audio('soundtrack2');
      
-     this.soundtrack2.play();
+     // this.soundtrack2.play();
      this.soundtrack1.play();
      
-     this.soundtrack2.loopFull();
+     // this.soundtrack2.loopFull();
      this.soundtrack1.loopFull();
  }
