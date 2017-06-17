31f244082b652080c609e8a6220d6e9ecc74c56e
Kai Chen <kaichenusesthis@gmail.com>
2017-06-17 05:49:51 +0000

    Added lives text

--- public/src/helpers/createText.js
+++ public/src/helpers/createText.js
@@ -3,4 +3,7 @@ var unitGame = unitGame || {};
 unitGame.Game.prototype.createText = function() {
     this.scoreText = this.game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#fff' });
     this.scoreText.fixedToCamera = true;
+    
+    this.livesText = this.game.add.text(400,16, 'Lives: 3', {fontSize: '32px', fill: '#fff'});
+    this.livesText.fixedToCamera = true;
 }
